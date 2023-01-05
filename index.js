fc796d0ab5d5f8fcf5cf564b6c4917397f33f461
Nazeer Haidari <76092148+Nazeer2020@users.noreply.github.com>
2023-01-05 10:51:37 +0430

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+const PrimaryHeader = document.querySelector(".primary-header");
+
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  PrimaryHeader.toggleAttribute("data-overlay");
+});
